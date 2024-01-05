366d3fa8e83538705a8b14a7e61ecee88cf22c9d
Austin Abell <austinabell8@gmail.com>
2024-01-04 22:24:33 -0500

    basic example of stateful program with pauses

--- methods/guest/src/main.rs
+++ methods/guest/src/main.rs
@@ -5,12 +5,21 @@ use risc0_zkvm::guest::env;
 risc0_zkvm::guest::entry!(main);
 
 pub fn main() {
-    // TODO: Implement your guest code here
-
+    let mut count = 0;
     loop {
+        println!("pre read");
         let input: u32 = env::read();
+        count += input;
         println!("input1: {input}");
+        println!("count: {count}");
         env::commit(&input);
-        env::pause(1);
+        env::pause(0);
+
+        let input: u32 = env::read();
+        count += input;
+        println!("input2: {input}");
+        println!("count: {count}");
+        env::commit(&input);
+        env::pause(0);
     }
 }
